859a32c26d1578dae926c9418ce0ddc5bb3657a6
MIT-FS <irm@us.es>
2026-03-02 12:36:55 +0100

    problema licencia apache

--- src/main/java/us/dit/ueba/soar/services/openc2/OpenC2Consumer.java
+++ src/main/java/us/dit/ueba/soar/services/openc2/OpenC2Consumer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Universidad de Sevilla/Departamento de Ingeniería Telemática
+ * Copyright 2026 Universidad de Sevilla/Departamento de Ingeniería Telemática
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
